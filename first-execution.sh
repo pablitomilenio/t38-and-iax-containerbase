0d42b8d3c3ac434809851bcd90b96c5723474741
Pablo Escaida <1pablo.escaida1@gmail.com>
2022-12-28 16:52:11 +0100

    working a bit better. Vpnc timeout

--- first-execution.sh
+++ first-execution.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-exec vpnc fritzbox &
 exec service iaxmodem start &
 exec iaxmodem ttyIAX0 &
 exec service asterisk start &
 exec service hylafax start &
 exec asterisk &
 exec asterisk &
-exec service dbus start &
-exec service network-manager start &
+exec vpnc fritzbox &
+# exec service dbus start &
+# exec service network-manager start &
 exec node /home/root/website/server.js
