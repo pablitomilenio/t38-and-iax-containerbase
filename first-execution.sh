d2d9b1a0ce4de4a32587c4ff9d71ee9b1e2b4217
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-01-03 10:13:16 +0100

    increased privileges

--- first-execution.sh
+++ first-execution.sh
@@ -4,7 +4,7 @@ exec service iaxmodem start &
 exec iaxmodem ttyIAX0 &
 exec service asterisk start &
 exec service hylafax start &
-exec service ike start &
+# exec service ike start &
 exec asterisk &
 exec asterisk &
 # exec vpnc fritzbox &
@@ -12,5 +12,5 @@ exec asterisk &
 # exec service network-manager start &
 # exec service ipsec start &
 # exec ipsec up fritzbox &
-exec ikec -r winfritz.vpn -u pablo.escaida -p Fr@Q319j@zML -a &
+# exec ikec -r winfritz.vpn -u pablo.escaida -p Fr@Q319j@zML -a &
 exec node /home/root/website/server.js
